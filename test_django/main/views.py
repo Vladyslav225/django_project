afa8bea3d4a073211d5027dac6157f65adf5950a
Vlaadyslav225 <yvv.work@gmail.com>
2022-01-06 15:07:49 +0200

    .

--- test_django/main/views.py
+++ test_django/main/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
-
+from .models import Task
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'main/index.html')
+    task = Task.objects.order_by('-id') # Sorting by a certain field
+    return render(request, 'main/index.html', {'task': task, 'title': 'Главная страница'})
 
 
 def about(request):
     return render(request, 'main/about.html')
+
+
+def pricing(request):
+    return render(request, 'main/pricing.html')
