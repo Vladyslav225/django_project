f5baa935f790c704c802ad792d384a4f8a1b0945
Yarynovskiy Vladyslav <64095441+Vladyslav225@users.noreply.github.com>
2022-02-01 17:01:11 +0200

    Update tasks.py

--- test_django/main/tasks/tasks.py
+++ test_django/main/tasks/tasks.py
@@ -1,17 +1,2 @@
 from celery import shared_task
 from test_django.celery import app
-
-
-@app.task(bind=True)
-def test_celery_func(*args, **kwargs):
-    print('qqqqqq')
-    return None
-
-
-@shared_task
-def task_celery(*args, **kwargs):
-    print('wwwwww')
-    for item in range(10):
-        print(item)
-
-    return None
